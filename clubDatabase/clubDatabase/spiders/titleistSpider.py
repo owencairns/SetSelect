9b6df9ee630af78bff2901b3d18a6a78e8bb06a8
owencairns <owencairns@me.com>
2023-01-10 19:27:57 -0500

    simplified titleist club spider

---
+++ clubDatabase/clubDatabase/spiders/titleistSpider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class TitleistClubLinks(scrapy.Spider):
+    name = 'titleistSpider'
+    allowed_domains = ['titleist.com']
+    start_urls = ['https://www.titleist.com/golf-clubs/']
+
+    def parse(self, response):
+        for link in response.css('div.product-tile-details a::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_club)
+
+    def parse_club(self, response):
+        club_name = response.css('h1.product-name::text').get()
+        price = response.css('span.price-sales::text').get()
+        link = response.url
+
+        yield {
+            'name': club_name.strip(),
+            'price': price.strip(),
+            'link': link
+        }
